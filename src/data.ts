bc16bbba46e8cf25f82909a4b43f581ddf19ac3f
ubaumann <github@m.ubaumann.ch>
2023-02-03 18:36:48 +0100

    Add menu items without pictures

--- src/data.ts
+++ src/data.ts
@@ -12,9 +12,12 @@ type Data = {
 export const data: Data = {
     menuItems: [
         {
-            name: "Bier",
-            price: 5,
-            image: lagerImg,
+            name: "Mineral (Mit)",
+            price: 4,
+        },
+        {
+            name: "Mineral (Ohne)",
+            price: 4,
         },
         {
             name: "Cola",
@@ -22,9 +25,85 @@ export const data: Data = {
             image: colaImg
         },
         {
-            name: "Mineral",
-            price: 3.5,
-            // image: colaImg
-        }
+            name: "Cola Zero",
+            price: 4,
+        },
+        {
+            name: "Citro",
+            price: 4,
+        },
+        {
+            name: "Rivella rot",
+            price: 4,
+        },
+        {
+            name: "Schorle",
+            price: 4,
+        },
+        {
+            name: "Eistee citrone",
+            price: 4,
+        },
+        {
+            name: "Schützengarten Lager",
+            price: 5,
+            image: lagerImg,
+        },
+        {
+            name: "Schützengarten Panache",
+            price: 5,
+        },
+        {
+            name: "Schützengarten Grapefruit",
+            price: 5,
+        },
+        {
+            name: "Schützengold (alkoholfrei)",
+            price: 5,
+        },
+        {
+            name: "Saft vom Fass (Mit)",
+            price: 6.50,
+        },
+        {
+            name: "Saft vom Fass (Ohne)",
+            price: 6.50,
+        },
+        {
+            name: "Herbstgold Weiss 10cl",
+            price: 6,
+        },
+        {
+            name: "Herbstgold Weiss 50cl",
+            price: 21,
+        },
+        {
+            name: "Herbstgold Rot 10cl",
+            price: 6,
+        },
+        {
+            name: "Herbstgold Rot 50cl",
+            price: 21,
+        },
+        {
+            name: "Kaffee",
+            price: 3.50,
+        },
+        {
+            name: "Kaffee Lutz",
+            price: 6.50,
+        },
+        {
+            name: "Kaffee Biberflade",
+            price: 6.50,
+        },
+        {
+            name: "Appenzeller",
+            price: 6,
+        },
+        {
+            name: "Appenzeller Rahmlikör",
+            price: 6,
+        },
     ]
 }
